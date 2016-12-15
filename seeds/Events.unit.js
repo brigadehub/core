f3e1e4a59d049aa51b949d2173bf5974739ecdc4
Trent Oswald <trentoswald@therebelrobot.com>
2016-12-14 19:10:29 -0800

    test(seeds): fixed event seed test

--- seeds/Events.unit.js
+++ seeds/Events.unit.js
@@ -1,13 +1,12 @@
 import test from 'ava'
 import _ from 'lodash'
 
-const EventsSeed = require('./Events')
+const events = require('./Events')
 
 test('Events seed should return an array of one example events', (t) => {
-  let events = EventsSeed()
   t.is(_.isArray(events), true, 'Events seed returns an array')
   t.is(events.length, 3, 'Events seed array contains three objects')
-  const event = event[0]
+  const event = events[0]
   t.is(_.isObject(event), true, 'Events seed array [0] is an object')
   const eventName = 'Event 1'
   t.is(event.title, eventName, `Events returned is ${eventName}`)
