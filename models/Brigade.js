3c9ae94bbe1b3b0966c671e32fcc591a334f76fe
Trent Oswald <trentoswald@therebelrobot.com>
2016-12-29 14:02:43 -0800

    chore(model): testing brigade presave

--- models/Brigade.js
+++ models/Brigade.js
@@ -7,6 +7,14 @@ var getProp = require('@f/get-prop')
 var brigadeSchema = require('../schemas/brigade')
 brigadeSchema.pre('save', function (next) {
   const brigade = this
+  console.log(
+    passport,
+    getProp('_strategies', passport),
+    getProp('_strategies.github', passport),
+    getProp('_strategies.github._callbackURL', passport),
+    getProp('_strategies.github._oauth2._clientId', passport),
+    getProp('_strategies.github._oauth2._clientSecret', passport)
+  )
   if (getProp('_strategies.github._oauth2._clientId', passport)) {
     passport._strategies.github._oauth2._clientId = brigade.auth.github.clientId
     passport._strategies.github._oauth2._clientSecret = brigade.auth.github.clientSecret
