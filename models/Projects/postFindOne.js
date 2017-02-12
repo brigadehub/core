c991b3065ec6a59899bbd63d4e3888f0ccb9f91d
Trent Oswald <trentoswald@therebelrobot.com>
2017-02-12 14:49:34 -0800

    :bug: fixed issue with not finding a project model

--- models/Projects/postFindOne.js
+++ models/Projects/postFindOne.js
@@ -1,5 +1,6 @@
 const fetchContributors = require('./fetchContributors')
 module.exports = function projectsPostFindOne (project, next) {
+  if (!project) return next(null, project)
   fetchContributors(project).then(function (results) {
     project.leads = results.leads
     project.members = results.members
